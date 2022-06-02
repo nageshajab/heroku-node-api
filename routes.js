90c2ec0f7b9baa27e3a0014079fde7acaa34d129
Nagesh Ajab <najab@halfpenny.com>
2022-06-02 22:08:56 +0530

    added routes.js

--- routes.js
+++ routes.js
@@ -4,6 +4,10 @@ const middleware = require('./middleware');
 
 module.exports = function (app) {
 
+    app.get('/',function(req,res){
+        res.send('api is up and running....');
+    });
+
     //user routes - anonymous access
     app.post("/user/generateToken", function (req, res) {
         usercontroller.generateToken(req, res);
@@ -12,6 +16,7 @@ module.exports = function (app) {
 
     //define routes for watchList
     app.post('/watchlist/list',middleware.validateJwt, function (req, res) {
+        console.log('inside api watch list route');
         watchlistController.list(req, res);
     });
     app.post('/watchlist/insert', middleware.validateJwt, function (req, res) {        
